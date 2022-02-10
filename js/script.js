299c21f9498f84337cd57e58a9be74177d3a1090
Prizext <98367452+Prizext@users.noreply.github.com>
2022-02-10 13:39:46 -0300

    Atualização de menu

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
-var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024); 
-
-document.querySelector("button").onclick = () =>{
-db.transaction(function (tx, result) { 
-   var usr = document.getElementById("usuario").value; 
-   var senha = document.getElementById("senha").value; 
-   tx.executeSql('CREATE TABLE IF NOT EXISTS logins (id unique, senha)'); 
-   tx.executeSql('INSERT INTO logins (id, senha) VALUES (? ,?)',
-                    [usr,senha], 
-                    function(tx,result){console.log(result)},
-                    function(tx,result){console.log(result)} ); 
-   
-   
-   console.log(result)
-})
-
-}
+$(".drop")
+  .mouseover(function() {
+  $(".dropdown").show(300);
+});
+$(".drop")
+  .mouseleave(function() {
+  $(".dropdown").hide(300);     
+});
